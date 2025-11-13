aa3bfb964ea0138ff4521a2ebde605bdb3cc4081
Nabil Lehlou <nlehlou@gmail.com>
2025-11-13 17:39:02 +0100

    Enable the appendage of a suffix to the version of built NPM packages

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # About this repo
 This repo hosts automation scripts.
+
+# CI/CD Environment Variables
+
+## NPM
+- `NPM_VERSION_PREID` is optional, and can hold the suffix added to the version of the built NPM packages.
--- src/cicd/npm/lib/push.sh
+++ src/cicd/npm/lib/push.sh
@@ -11,4 +11,5 @@ log_into_npm
 
 cd ${PROJECT_DIR}
 [ ! -z "${DOCKER_ARTIFACT_SUBDIR}" ] && cd ${DOCKER_ARTIFACT_SUBDIR} || true
+[ ! -z "${NPM_VERSION_PREID}" ] && sed -E -i "s/(\"version\": \"[^\"]*)(\")/\1-${NPM_VERSION_PREID}\2/" package.json
 [ ! -z "${NPM_REPO_ACCESS}" ] && npm publish --access ${NPM_REPO_ACCESS} || npm publish
